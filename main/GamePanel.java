b6b5c008a131e05c5204134b1fb42fc710e23894
ngocminh1605 <100121817+ngocminh1605@users.noreply.github.com>
2022-09-30 23:54:29 +0700

    Update GamePanel.java

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -17,11 +17,12 @@ import javax.swing.JPanel;
 import tile.TileManager;
 
 /**
- *
+ * GamePanel là tác động nội dung trên màn hình
  * @author Lenovo
  */
 public class GamePanel extends JPanel implements Runnable{
     // screen settings
+    // cài đặt màn hình
     final int WINDOW_WIDTH = 1200;
     final int WINDOW_HEIGHT = 720;
     
@@ -31,17 +32,24 @@ public class GamePanel extends JPanel implements Runnable{
     public int maxRows = 15;
     
     // time and fps handling stuffs
+    // tính thời gian và fps
     private long thisTime, lastTime;
     private final int FPS = 60;
     private final float drawInterval = 1000 / FPS;
     
+    // sự kiện đầu vào bàn phím
     KeyHandler input = new KeyHandler();
+    
+   // luồng bắt đầu
     Thread gameThread;
     Player player = new Player(this, input);
+    // quản lý map
     TileManager tileManager = new TileManager(this);
+    // đọc ảnh bg
     BufferedImage bg;
+    // check va chạm 
     public CollisionChecker cChecker = new CollisionChecker(this);
-    
+    //  GamePanel là tác động nội dung trên màn hình
     public GamePanel() {
         this.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         this.setBackground(Color.black);
@@ -63,6 +71,7 @@ public class GamePanel extends JPanel implements Runnable{
     
     @Override
     public void run() {
+        // xử lý thời gian 
         lastTime = System.currentTimeMillis();
         double delta = 0;
         
@@ -84,16 +93,18 @@ public class GamePanel extends JPanel implements Runnable{
     public void update() {
         player.update();
     }
-    
+    // hàm vẽ
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        // vẽ bg
         g.drawImage(bg, 0, 0, null);
-        
+  
         Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.WHITE);
-        
+        // vẽ map
         tileManager.draw(g2);
+        // vẽ nhân vật
         player.draw(g2);
         
         g2.dispose();
